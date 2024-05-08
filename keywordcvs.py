149aaa73832c4a67b554220d907c8bd9bcf8859b
Ngan Nguyen <35237637+Ngann@users.noreply.github.com>
2024-05-08 14:32:22 -0700

    Create keywordcvs.py

---
+++ keywordcvs.py
@@ -0,0 +1,15 @@
+import requests
+
+# Azure DevOps Personal Access Token
+token = 'YOUR-PERSONAL-ACCESS-TOKEN'
+
+# Azure DevOps organization URL and project name
+org_url = 'https://dev.azure.com/YourOrganization'
+project_name = 'YourProject'
+
+# Keyword to search
+keyword = 'your_keyword'
+
+# Headers for API requests
+headers = {
+    'Authorization': 'Basic
